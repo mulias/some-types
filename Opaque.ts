1c26f457b44384da909ad6dd62858a0e8daedc5f
Elias Mulhall <eli.mulhall@gmail.com>
2022-04-05 14:10:23 -0400

    Run formater

--- Opaque.ts
+++ Opaque.ts
@@ -1,5 +1,5 @@
-export { Opaque, T }
+export { Opaque, T };
 
-type Opaque<A, Brand> = A & { readonly __opaque__: Brand};
+type Opaque<A, Brand> = A & { readonly __opaque__: Brand };
 
-type T<A, Brand> = Opaque<A, Brand>
+type T<A, Brand> = Opaque<A, Brand>;
